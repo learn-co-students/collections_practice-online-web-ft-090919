c4914672949730b6729805eb5579ffbaa09df481
Dean L Betty <deanlloyd64@gmail.com>
2019-09-12 19:40:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+    
+def add_s(array)
+  plural = Array.new
+  array.each do |word|
+    plural << word + "s"
+  end
+  plural[1].slice!("s")
+  plural
+end
