6b0a5a65e7ab4a9c618e051d6c0e605c46d27385
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-12 16:22:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
 
+def sort_array_asc (array)
+  array.sort
+end
+
+def sort_array_desc (array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count (array)
+  array.sort_by {|obj| obj.length}
+end
+
+def swap_elements (array)
+  array_new = array
+  array_new[1], array_new[2] = array_new[2], array_new[1]
+  array_new
+end
+
+def reverse_array (array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  array.map do |element|
+    element_array = element.split(//)
+    element_array[2] = "$"
+    element_array.join
+  end
+end
+
+def find_a (array)
+   array.select {|string| string.split(//)[0] == "a"}
+end
+
+def sum_array (array)
+  array.sum
+end
+
+def add_s (array)
+  array.collect.with_index {|string, index| index == 1? string : string << "s"}
+end
