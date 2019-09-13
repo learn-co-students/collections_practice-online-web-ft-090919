b8d4eb73b0f641ad7a35b729567b22f2c89fe0e4
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-13 20:34:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(arr)
+arr.sort_by{|word| word.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.find_all do |word|
+  word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    if word[0].upcase == "a".upcase
+    p word
+  end
+end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word|
+    if word != array[1]
+      word = word + "s"
+    else 
+      word  
+    end
+  end
+end
